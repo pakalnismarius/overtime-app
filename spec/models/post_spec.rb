b4f5705e04c9685853b4f0b1f6fb46408cf40da8
marius <pakalnis.marius@gmail.com>
2017-03-02 10:10:35 +0200

    Used resource generator for post creation

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe "creattion" do
+
+    before do
+      @post = Post.create(date: Date.today, rationale: "Anyting")
+    end
+
+    it "can be created" do 
+      expect(@post).to be_valid
+    end
+
+    it "Should not be created without date and rationale" do 
+      post = Post.create()
+      expect(post).to_not be_valid
+    end
+  end
+end
