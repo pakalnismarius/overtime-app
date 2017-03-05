1b58229118b6922d22073467de991234df619e60
marius <pakalnis.marius@gmail.com>
2017-03-06 00:42:57 +0200

    Implemented index view with posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe "creattion" do
+  describe "creation" do
 
     before do
-      @post = Post.create(date: Date.today, rationale: "Anyting")
+      user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Dzo", last_name: "Travianis")
+      @post = Post.create(date: Date.today, rationale: "Some content", user_id: user.id)
     end
 
-    # it "can be created" do 
-    #   expect(@post).to be_valid
-    # end
+    it "can be created" do 
+      expect(@post).to be_valid
+    end
 
     it "Should not be created without date and rationale" do 
       post = Post.create()
