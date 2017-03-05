228d73ccad92125c0e0bda14965356036d4bc602
marius <pakalnis.marius@gmail.com>
2017-03-06 00:40:58 +0200

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+@user = User.create(first_name: "ma", last_name: "pa", email: "ma@pa.lt", password: "123456", password_confirmation: "123456")
+puts "created one user"
+
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content")
+  Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "work is done"
