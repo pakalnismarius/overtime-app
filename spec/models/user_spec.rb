3457e90c2ae65dc3a745e19689f82c25a799901a
marius <pakalnis.marius@gmail.com>
2017-03-06 02:03:43 +0200

    Implemented xustom full name method to user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+      
+  before do 
+    @user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Dzo", last_name: "Travianis")
+  end
+
   describe "creation" do
-    before do 
-      @user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Dzo", last_name: "Travianis")
-    end
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -13,6 +15,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custom name method" do
+    it "has a method that combines first_name and last_name" do
+      expect(@user.full_name).to eq("TRAVIANIS, DZO")
     end
   end
 end
