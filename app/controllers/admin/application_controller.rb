42e53eec834c374b0591c36fc69f1b6a70e4eb7c
marius <pakalnis.marius@gmail.com>
2017-03-12 21:20:01 +0200

    implemented admin access to admin dashboard

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -12,15 +12,14 @@ module Admin
 
   class ApplicationController < Administrate::ApplicationController
     before_action :authenticate_user!
-    before_filter :authenticate_admin
+    before_action :authenticate_admin
 
     def authenticate_admin
       unless Admin.admin_types.include?(current_user.try(:type))
-        flash[:alert] = "You are not authorized to access this page."
+        flash[:alert] = "You are not authorized to acces this page."
         redirect_to(root_path)
       end
     end
-
     # Override this value to specify the number of elements to display at a time
     # on index pages. Defaults to 20.
     # def records_per_page
