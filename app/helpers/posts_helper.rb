bd2eb6fe2cdfa1a1651cc072f2f1de6991501868
marius <pakalnis.marius@gmail.com>
2017-03-14 12:54:15 +0200

    Implemented labels with status values on index for posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,19 @@
 module PostsHelper
+  
+  def status_label status
+    status_spam_generator status
+  end
+
+  private
+
+  def status_spam_generator status
+    case status
+      when "submitted"
+      content_tag(:spam, status.titleize, class: 'label label-primary')
+      when "approved"
+      content_tag(:spam, status.titleize, class: 'label label-success')
+      when "rejected"
+      content_tag(:spam, status.titleize, class: 'label label.titleize-danger')
+    end
+  end
 end
