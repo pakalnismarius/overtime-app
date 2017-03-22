8050da66c1de35ec52f99986daa8194c654ec22d
marius <pakalnis.marius@gmail.com>
2017-03-22 10:02:38 +0200

    Integrated phone validations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -10,7 +10,7 @@ FactoryGirl.define do
     email { generate :email }
     password "123456"
     password_confirmation "123456"
-    phone "370666"
+    phone "3706660000"
   end
 
   factory :admin_user, class: "AdminUser" do
@@ -19,7 +19,7 @@ FactoryGirl.define do
     email { generate :email }
     password "123456"
     password_confirmation "123456"
-    phone "3706669"
+    phone "3706669000"
   end
 
   factory :non_authorized_user, class: "User" do
@@ -28,7 +28,7 @@ FactoryGirl.define do
     email { generate :email }
     password "123456"
     password_confirmation "123456"
-    phone "3706669"
+    phone "3706669000"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,9 @@ RSpec.describe User, type: :model do
     it "can be created" do
       expect(@user).to be_valid
     end
+  end
 
+  describe "validations" do
     it "can not be created without first_name" do
       @user.first_name = nil
       expect(@user).to_not be_valid
@@ -23,6 +25,16 @@ RSpec.describe User, type: :model do
 
     it "can not be created without phone" do
       @user.phone = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'requires the phone attr to only contain integers' do
+      @user.phone = 'mygreatstr'
+      expect(@user).to_not be_valid
+    end
+
+    it 'requires thr phone attr only have 10 chars' do
+      @user.phone = '12345678901'
       expect(@user).to_not be_valid
     end
   end
