bb0a16341fd9ea6d6541e452a04ea3c994f0675b
marius <pakalnis.marius@gmail.com>
2017-03-29 09:55:08 +0300

    Blocked non admin users from accessing index page for audits

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -18,5 +18,15 @@ describe 'AuditLog features' do
       visit audit_logs_path
       expect(page).to have_content(/PA/)
     end
+
+    it 'cannot be accessed by non admin users' do
+      logout(:user)
+      user = FactoryGirl.create(:user)
+      login_as(user, :scope => :user)
+
+      visit audit_logs_path
+
+      expect(current_path).to eq(root_path)
+    end
   end
 end
