eb274183b3bf749af755742354b8b69d4a82161d
marius <pakalnis.marius@gmail.com>
2017-03-29 09:44:34 +0300

    Implemented initial view for audit logs

---
+++ spec/features/audit_log_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'AuditLog features' do
+
+  describe 'index' do 
+
+    before do
+      admin_user = FactoryGirl.create(:admin_user)
+      login_as(admin_user, :scope => :user)
+      FactoryGirl.create(:audit_log)
+    end
+    it 'has an inde page that can be reached' do
+      visit audit_logs_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'renders audit_log content' do
+      visit audit_logs_path
+      expect(page).to have_content(/PA/)
+    end
+  end
+end
